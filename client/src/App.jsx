c91f2f13a18c28394ce6b66a12066c9ca4f32c97
danieskka <daniescalera93@hotmail.com>
2023-07-24 15:09:07 +0200

    fix/cookie

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,28 +8,48 @@ function App() {
 
   const handleSignup = async (data) => {
     try {
-      await axios.post("http://localhost:3000/signup", data);
+      await axios.post("/signup", data);
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
+
+  const handleLogin = async (data) => {
+    try {
+      await axios.post("/login", data);
     } catch (error) {
       console.log("Error:", error);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit(handleSignup)} className="sign-up-form">
-      <label>Email:</label>
-      <input type="email" {...register("email", { required: true })} />
+    <>
+      <form onSubmit={handleSubmit(handleSignup)} className="sign-up-form">
+        <label>Email:</label>
+        <input type="email" {...register("email", { required: true })} />
 
-      <label>Password:</label>
-      <input type="password" {...register("password", { required: true })} />
+        <label>Password:</label>
+        <input type="password" {...register("password", { required: true })} />
 
-      <label>Age:</label>
-      <input type="number" {...register("age", { required: true })} />
+        <label>Age:</label>
+        <input type="number" {...register("age", { required: true })} />
 
-      <label>Location:</label>
-      <input type="text" {...register("location", { required: true })} />
+        <label>Location:</label>
+        <input type="text" {...register("location", { required: true })} />
 
-      <button type="submit">Sign up</button>
-    </form>
+        <button type="submit">Sign up</button>
+      </form>
+
+      <form onSubmit={handleSubmit(handleLogin)}  className="sign-up-form">
+        <label>Email:</label>
+        <input type="email" {...register("email", { required: true })} />
+
+        <label>Password:</label>
+        <input type="password" {...register("password", { required: true })} />
+
+        <button type="submit">Log in</button>
+      </form>
+    </>
   )
 }
 
