ee31a5a9043ff2a31b5d3aad40de30f8c405b493
DeveKoke <jorge.be86@gmail.com>
2023-07-24 12:37:08 +0200

    routes dom Main

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import {Routes, Route} from 'react-router-dom';
+import Home from './Home/Home';
+
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+  <main>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+  </main>
+)
 };
 
 export default Main;
