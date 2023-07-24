91580333209748f832517cf274c251f5f7834958
DeveKoke <jorge.be86@gmail.com>
2023-07-24 15:09:26 +0200

    map leaflet inserted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
 
 import './App.css'
+import Main from './components/Main';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
 
 
   return (
     <>
-  <p>hola</p>
+    <BrowserRouter>
+      <Header />
+      <Main />
+      <Footer />
+    </BrowserRouter>
     </>
   )
 }
