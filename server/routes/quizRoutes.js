c05f3376e642610b38e5851899c253370f763e24
danieskka <daniescalera93@hotmail.com>
2023-07-26 15:56:00 +0200

    fix/branches2

--- server/routes/quizRoutes.js
+++ server/routes/quizRoutes.js
@@ -3,5 +3,6 @@ const quizRouter = express.Router();
 const quizController = require('../controllers/quizController');
 
 quizRouter.post("/quiz", quizController.saveAnswers);
+quizRouter.get("/quiz", aut)
 
 module.exports = quizRouter;
