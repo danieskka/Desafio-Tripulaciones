5e084d3646e12579b418302593dd33e8ab4da4cc
DeveKoke <jorge.be86@gmail.com>
2023-07-27 16:57:06 +0200

    home gallery

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import Main from './components/Main';
 import Header from './components/Header';
-import Footer from './components/Footer';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <BrowserRouter>
       <Header />
       <Main />
-      <Footer />
     </BrowserRouter>
   )
 }
--- client/src/components/Main/Home/Home.jsx
+++ client/src/components/Main/Home/Home.jsx
@@ -6,35 +6,48 @@ import { useState } from "react";
 
 const Home = () => {
 
-  const printNews = () => news.map(item => <Gallery head={item.headline} img={item.img} url={item.url}  key={uuidv4()} />)
+  // Paginación 
+  const itemsPage = 1; 
+  const [currentPage, setCurrentPage] = useState(1); 
+  const totalPages = Math.ceil(news.length / itemsPage)
+  //Pintar paginación
+  const printNews = () => {
+    const startIndex = (currentPage - 1) * itemsPage;
+    const endIndex = startIndex + itemsPage;
+    return news.slice(startIndex, endIndex).map(item => (
+      <Gallery head={item.headline} img={item.img} url={item.url} key={uuidv4()} />
+    ));
+  }
 
-  const [currentPage, setCurrentPage] = useState(1); // Estado para rastrear la página actual
-
+  //Botones paginación
   const goToPreviousPage = () => {
     if (currentPage > 1) {
       setCurrentPage(currentPage - 1);
     }
   };
-  
-  const goToNextPage = () => {
+    const goToNextPage = () => {
+    if (currentPage < totalPages) {
       setCurrentPage(currentPage + 1);
+    }
   };
 
 
 
   return (
-    <section>
+    <section className="homeWrapper">
         <h1>Hola ( NOMBRE DE USUARIO )</h1>
-      <article>
+      <article className="progressHomeContainer">
         <h2>Mira tu progreso</h2>
         <img className="progressMapGame" src="https://img.freepik.com/vector-premium/mapa-niveles-juego-espacial-planetas-estrellas-fondo-espacio-botones-kit-interfaz-usuario-aplicacion-progreso-jugador_596401-437.jpg?w=2000" alt="Progreso" />
         <button>SEGUIR APRENDIENDO</button>
       </article>
-      <article>
+      <article className="galleryHomeContainer">
         <h2>Para ti</h2>
+        <div className="gallery">
+          <img className="galleryButtons" src="assets/ArrowL.png" alt="Artículo anterior" onClick={goToPreviousPage} disabled={currentPage === 1}/>
         {printNews()}
-        <button onClick={goToPreviousPage}>Atrás</button>
-        <button onClick={goToNextPage}>Siguiente</button>
+          <img className="galleryButtons" src="assets/ArrowR.png" alt="Artículo siguiente" onClick={goToNextPage} disabled={currentPage === totalPages} />
+        </div>
       </article>
     </section>
   );
