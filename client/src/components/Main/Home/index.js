adc04abae9fb84a48ce8f83e71f0b5a956972ef2
DeveKoke <jorge.be86@gmail.com>
2023-07-31 15:43:01 +0200

    slider home done

--- client/src/components/Main/Home/index.js
+++ client/src/components/Main/Home/index.js
@@ -6,6 +6,11 @@ export const news = [
         url: 'https://www.20minutos.es/noticia/5159691/0/incendios-portugal-espana-europa-en-directo-ultimas-noticias/'
     },
     {
+        headline: 'Mapa de calor',
+        img: 'assets/Screenshot_240.png',
+        url: '/zone'
+    },
+    {
         headline: 'Boletín de información de Olas de Calor',
         img: 'https://lalunadelhenares.com/wp-content/uploads/2020/07/continua-ola-calor-temperaturas-no-tregua-miercoles-corredor-noticias-valle-henares-1024x682.jpg',
         url: 'https://www.comunidad.madrid/servicios/salud/boletin-informacion-olas-calor'
