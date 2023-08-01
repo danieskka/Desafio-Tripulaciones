739467e9b6ab9288638e2e5f5770f47a0bc1eaba
danieskka <daniescalera93@hotmail.com>
2023-08-01 19:59:15 +0200

    add/profile user

--- client/src/components/Main/Home/Profile/User/User.jsx
+++ client/src/components/Main/Home/Profile/User/User.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import axios from 'axios';
+import Img28 from "../../../../../../public/assets/Frame 28.svg";
+import LogOut from "../../../../../../public/assets/LogOutButton.svg";
 
 const User = () => {
-  return <div>User</div>;
+
+  const handleLogOut = async () => {
+    try {
+      await axios.post('/logout');
+    } catch (error) {
+      console.log('Error during logout:', error);
+    }
+  };
+
+  return (
+    <>
+    <h1>Ajustes</h1>
+    <section className="container">
+      <article className="article-container">
+        <img src={Img28} className="img28"></img>
+      </article>
+      <article className="article-container">
+        <img src={LogOut} className="logoutimg" onClick={handleLogOut}></img>
+      </article>
+    </section>
+    </>
+  )
 };
 
 export default User;
