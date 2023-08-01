11412d82ebcc13ff207c87e5581fabf9c1a15b0a
atrislab <atrislab@gmail.com>
2023-08-01 14:43:39 +0200

    scss changes in game

--- client/src/components/Main/Home/Game/Game.jsx
+++ client/src/components/Main/Home/Game/Game.jsx
@@ -1,19 +1,51 @@
 import {Link} from "react-router-dom";
-import Navegation from '../../Navegation/Navegation';
+
 
 
 const Game = () => {
   return (
     <>
-      <section className="homeWrapper">
-        <h1>Tu progreso</h1>
-        <article></article>
-
+      <section className="game_general_container">
+      
+      <article className="game_title_quiz">
+        <h1>Quiz</h1>
+      </article>
+      <article className="game_bg_quiz">
         {/* <Link to="/minilecture"><article className="link_to_minilecture"></article></Link> */}
-          <img className='link_to_minilecture' src="../../../public/assets/Camino.svg" />
-        
+        <img src="../../../public/assets/Camino.svg" />
+      </article>
+
+      
+    <div className="nav_fixed">
+      <Link to="/home">        
+        <div className="iconNavigation">
+          <img src="assets/Home.svg" alt="home" />
+          <span>Home</span>
+        </div>
+      </Link>
+      <Link to="/game">        
+        <div className="iconNavigation">
+        <img src="assets/juego.svg" alt="juego" />
+          <span>Juego</span>
+        </div>
+      </Link>
+      <Link to="/info">        
+        <div className="iconNavigation">
+        <img className="formacion" src="assets/Formación.svg" alt="formación" />
+          <span>Formación</span>
+        </div>
+      </Link>
+      <Link to="/profile">  
+        <div className="iconNavigation">
+          <img src="assets/perfil.svg" alt="perfil" />
+          <span>Perfil</span>
+        </div>
+      </Link>
+      </div>
+      
+
       </section>
-      <Navegation />
+     
 
     </>
   );
