efa9319a15e9cd4cfc0ab4ab6b805bb4fc51e8ba
danieskka <daniescalera93@hotmail.com>
2023-08-02 14:02:09 +0200

    fix styles and navigation signup

--- client/src/components/Main/Welcome/Welcome.jsx
+++ client/src/components/Main/Welcome/Welcome.jsx
@@ -5,6 +5,10 @@ import axios from "axios";
 
 import RegisterSucc from "../../../../public/assets/PICTURE 10.jpg";
 import HeatyLogo from "../../../../public/assets/HeatyLogo.png";
+import ForgotPassword from "../../../../public/assets/Olvidé mi contraseña.svg";
+import RemindMe from "../../../../public/assets/Recuérdame.svg";
+import Register from "../../../../public/assets/Frame 33.svg";
+import Admin from "../../../../public/assets/Ingresar como administrador.svg";
 
 const Welcome = () => {
 
@@ -23,13 +27,13 @@ const Welcome = () => {
   return (
     <>
           {isLogin ? ( 
-        <section className="login-container">
-          <article className="login-success">
-            <p>¡Has iniciado sesión con éxito!</p>
-            <img src={RegisterSucc} alt="Mensaje de inicio de sesión" />
-            <Link to="/home" className="home-link">Ir al inicio</Link>
-          </article>
-        </section>
+            <section className="login-container">
+              <article className="login-success">
+                <p>¡Has iniciado sesión con éxito!</p>
+                <img src={RegisterSucc} alt="Mensaje de inicio de sesión" />
+                <Link to="/home" className="home-link">Ir al inicio</Link>
+              </article>
+            </section>
       ) : (
         <section className="login-container">
           
@@ -39,27 +43,28 @@ const Welcome = () => {
             <article className="login-success">
               
               <input type="email" {...register("email", { required: true })} placeholder="Correo electrónico" />
-
-              
               <input type="password" {...register("password", { required: true })} placeholder="Contraseña"/>
 
               <button type="submit" className="home-link">Iniciar Sesión</button>
             </article>
           </form>
-          
-        {/* </section> */}
-          
-            <article className="login-success">
 
+          <article className="first-article">
+            <img src={ForgotPassword}></img>
+            <img src={RemindMe}></img>
+          </article>
+            
+          <article className="other-article">
             <Link to="/register">
-              <p>¿No tienes cuenta? Regístrate</p>
+              <img src={Register}></img>
             </Link>
-            </article>
-            <article>
-              <Link to="/adminlogin">
-                <a>Ingresar como Administrador</a>
-              </Link>
-            </article>
+          </article>
+            
+          <article className="last-article">
+            <Link to="/adminlogin">
+              <img src={Admin}></img>
+            </Link>
+          </article>
         </section>
       )}
     </>
